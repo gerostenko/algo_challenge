14dadc8025540ecd9ca0a79e2fbadf54b2a11344
Galina Erostenko <gala.erostenko@cbc.ca>
2021-04-20 11:09:12 -0400

    comment out non-recursive insert solution for BinarySearchTree

--- src/ds/BinarySearchtree/index.js
+++ src/ds/BinarySearchtree/index.js
@@ -23,57 +23,24 @@ export class BinarySearchTree
         this.root = null;
     }
 
-    // insert(data){
-    //     const newNode = new BinaryTreeNode(data);
-    //     if(this.root === null) {
-    //         this.root = newNode;
-    //         return;
-    //     }
-    //     this.insertNode(this.root, newNode);
-
-    // }
-
-    // insertNode(node, newNode){
-    //     // let currentNode = newNode.data < node.data ? node.left : node.right;
-
-    //     if(newNode.data < node.data) {
-    //         node.left === null ? node.left = newNode : this.insertNode(node.left, newNode);
-    //     } else {
-    //         node.right === null ? node.right = newNode : this.insertNode(node.right, newNode);
-    //     }
-    // }
-
-
-    //insert without using recursion
     insert(data){
         const newNode = new BinaryTreeNode(data);
         if(this.root === null) {
             this.root = newNode;
             return;
         }
-
-        let current = this.root;
-        while (current !== null) {
-            if(data < current.data) {
-                if(current.left === null) {
-                    current.left = newNode;
-                    break;
-                } else {
-                    current = current.left;
-                }
-            } else if(data > current.data){
-                if(current.right === null) {
-                    current.right = newNode;
-                    break;
-                } else {
-                    current = current.right;
-                }
-            } else {
-                break;
-            }
-        }
-
+        this.insertNode(this.root, newNode);
     }
+
+    insertNode(node, newNode){
+        // let currentNode = newNode.data < node.data ? node.left : node.right;
+        if(newNode.data < node.data) {
+            node.left === null ? node.left = newNode : this.insertNode(node.left, newNode);
+        } else {
+            node.right === null ? node.right = newNode : this.insertNode(node.right, newNode);
+        }
+    }
+
 
     // remove(data)
     // Helper function
@@ -83,4 +50,35 @@ export class BinarySearchTree
     // preorder(node)
     // postorder(node)
     // search(node, data)
+
+    //insert without using recursion
+    // insert(data){
+    //     const newNode = new BinaryTreeNode(data);
+    //     if(this.root === null) {
+    //         this.root = newNode;
+    //         return;
+    //     }
+
+    //     let current = this.root;
+    //     while (current !== null) {
+    //         if(data < current.data) {
+    //             if(current.left === null) {
+    //                 current.left = newNode;
+    //                 break;
+    //             } else {
+    //                 current = current.left;
+    //             }
+    //         } else if(data > current.data){
+    //             if(current.right === null) {
+    //                 current.right = newNode;
+    //                 break;
+    //             } else {
+    //                 current = current.right;
+    //             }
+    //         } else {
+    //             break;
+    //         }
+    //     }
+
+    // }
 }
